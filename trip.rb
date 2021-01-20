09f9e3e17c86eead082d21dcb7c41c36a6f1216b
Tsuyoshi-Toughy <ogutsuyoshi@gmail.com>
2021-01-20 14:11:17 +0900

    インデントの見直し

--- trip.rb
+++ trip.rb
@@ -1,49 +1,47 @@
 puts <<~TEXT
-    旅行プランを選択して下さい。
-
-    1. 沖縄旅行(10000円)
-    2. 北海道旅行(20000円)
-    3. 九州旅行(15000円)
-
-TEXT
+  旅行プランを選択して下さい。
+  1. 沖縄旅行(10000円)
+  2. 北海道旅行(20000円)
+  3. 九州旅行(15000円)
+  TEXT
 
 while true
-    print "プランの番号を選択 >"
-    plan_num = gets.to_i
-    break if (1..3).include?(plan_num)
-    puts "1~3の番号を入力して下さい。"
+  print "プランの番号を選択 >"
+  plan_num = gets.to_i
+  break if (1..3).include?(plan_num)
+  puts "1~3の番号を入力して下さい。"
 end
 
 case plan_num
 
 when 1
-    place = "沖縄"
-    price = 10000
+  place = "沖縄"
+  price = 10000
 when 2
-    place = "北海道"
-    price = 20000
+  place = "北海道"
+  price = 20000
 when 3
-    place = "九州"
-    price = 15000
+  place = "九州"
+  price = 15000
 end
 
-    puts "#{place}旅行ですね。"
+puts "#{place}旅行ですね。"
 
-    puts "何名で予約されますか？"
+  puts "何名で予約されますか？"
 
 while true
-    print "人数を入力 >"
-    people_num = gets.to_i
-    break if people_num > 0
-    puts "1以上の入力して下さい。"
+  print "人数を入力 >"
+  people_num = gets.to_i
+  break if people_num > 0
+  puts "1以上の入力して下さい。"
 end
 
-    puts "#{people_num}名ですね。"
+puts "#{people_num}名ですね。"
 
 total_price = price * people_num
 
 if people_num >= 5
-    puts "5人以上なので10%割引になります。"
-    total_price *= 0.9
+  puts "5人以上なので10%割引になります。"
+  total_price *= 0.9
 end
-    puts "合計金額は#{total_price.floor}円になります。"
+  puts "合計金額は#{total_price.floor}円になります。"
